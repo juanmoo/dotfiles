7df42c0cf320d4d3f43e520547f714cbc05fa652
Juan Ortiz <juanmochoaortiz@gmail.com>
2024-01-21 20:51:12 -0800

    Updated sync script

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -5,12 +5,37 @@ LIVE_DIR="/home/juan"
 REPO_DIR=$1
 SYNC_CMD="rsync -Cau --exclude '*/.git/'"
 
-$SYNC_CMD ${LIVE_DIR}/.config/	 	${REPO_DIR}/.config
-$SYNC_CMD ${LIVE_DIR}/.mpd/	 	${REPO_DIR}/extras/mpd
-$SYNC_CMD ${LIVE_DIR}/.ncmpcpp/ 	${REPO_DIR}/extras/ncmpcpp
-$SYNC_CMD ${LIVE_DIR}/.fonts/	 	${REPO_DIR}/extras/fonts
-$SYNC_CMD ${LIVE_DIR}/.scripts/	 	${REPO_DIR}/extras/scripts
-$SYNC_CMD ${LIVE_DIR}/.oh-my-zsh/ 	${REPO_DIR}/extras/oh-my-zsh
+SUBDIRS="awesome
+alacritty
+btop
+calibre
+Cursor
+dconf
+gnome-session
+gtk-3.0
+ibus
+kitty
+libreoffice
+mpDris2
+ncmpcpp
+procps
+pulse
+Signal
+spotify
+systemd
+Thunar
+xfce4
+yay
+zathura"
 
-# TODO:
-# - create whitelist for .config repo
+for SUBDIR in $SUBDIRS
+do
+  $SYNC_CMD ${LIVE_DIR}/.config/${SUBDIR}/		${REPO_DIR}/.config/${SUBDIR}
+done
+
+#$SYNC_CMD ${LIVE_DIR}/.config/		${REPO_DIR}/.config
+$SYNC_CMD ${LIVE_DIR}/.mpd/		${REPO_DIR}/extras/mpd
+$SYNC_CMD ${LIVE_DIR}/.ncmpcpp/ 	${REPO_DIR}/extras/ncmpcpp
+$SYNC_CMD ${LIVE_DIR}/.fonts/		${REPO_DIR}/extras/fonts
+$SYNC_CMD ${LIVE_DIR}/.scripts/		${REPO_DIR}/extras/scripts
+$SYNC_CMD ${LIVE_DIR}/.oh-my-zsh/ 	${REPO_DIR}/extras/oh-my-zsh
